d467128ccef47704d8bf6dc535f14ae1a8c13672
Nicolas BAUW <nbauw@hotmail.com>
2020-06-26 22:40:20 +0200

    revert tag

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@
 //! tzparse = { version = "1.1", features=["json"] }
 //! ```
 //! 
-//! ```rust
+//! ```
 //! fn main() {
 //!     println!("{}", tzparse::get_zoneinfo("/usr/share/zoneinfo/Europe/Paris").unwrap().to_json().unwrap());
 //! }
